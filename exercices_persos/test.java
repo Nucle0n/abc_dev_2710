c6d23e5e8a85b54b3e0fa8fb25668af53f0d92a8
Nucleon <thibaud.guinchard@gmail.com>
2025-12-05 08:12:25 +0100

    exercice cours 1 terminé

---
+++ exercices_persos/test.java
@@ -0,0 +1,11 @@
+package abc_dev_2710.exercices_persos;
+
+import cours.techniques.scanner;
+
+public class test {
+public static void main(String[] args) {    
+    int note;
+    scanner sc = new scanner(System.in);
+
+    }
+}
