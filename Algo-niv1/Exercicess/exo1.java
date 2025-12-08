43b14cd083743c56d41336113c41e2bce17bd78e
Nucleon <thibaud.guinchard@gmail.com>
2025-12-08 08:51:17 +0100

    nettoage

---
+++ Algo-niv1/Exercicess/exo1.java
@@ -0,0 +1,40 @@
+/* EXO1 : A l'aide de Scanner et Print, me faire afficher le résultat d'une addition de deux entiers        
+ * 
+ * 
+* **********************AFFICHAGE ATTENDU ****************:
+ * 
+ * a = 2
+ * b = 3
+ * 
+ * resultat = a + b
+ * 
+ * resulat vaut 5
+ * 
+ * 
+ * 
+*/
+package Exercicess;
+
+import java.util.Scanner;
+
+public class exo1 {
+    public static void main(String[] args){
+    
+    // Définition des variables
+    Scanner sc = new Scanner(System.in);
+    int a, b, resultat;
+
+    System.out.print("a = ");
+    a  = sc.nextInt();
+
+    System.out.print("b = ");
+    b = sc.nextInt();
+
+    resultat = a + b;
+    System.out.println("Resultat = a + b");
+    System.out.println("Resultat vaut " + resultat);
+
+    sc.close();
+
+    }
+}
