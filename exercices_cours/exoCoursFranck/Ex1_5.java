5a25b5b51af8edbfe22b41be927905d67cf7a20f
Nucleon <thibaud.guinchard@gmail.com>
2025-12-15 15:52:51 +0100

    1_5

---
+++ exercices_cours/exoCoursFranck/Ex1_5.java
@@ -0,0 +1,18 @@
+package exoCoursFranck;
+import java.util.Scanner;
+public class Ex1_5 
+{
+    public static void main(String[] args)
+    {
+        //Definition des variables
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+
+        //Entrées des valeurs
+        System.out.print("Entrez une valeurpour A :");
+        a = sc.nextInt();
+        c = a
+        System.out.print("Entrez une valeurpour A :");
+        b = sc.nextInt();
+    }    
+}
